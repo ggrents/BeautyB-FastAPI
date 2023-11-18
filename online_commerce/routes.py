b9caecd029afcf309e4c451e440e5754bbb355bc
ggrents <chef_grents@mail.ru>
2023-11-18 11:39:34 +0600

    set up services and catalogue pages

--- online_commerce/routes.py
+++ online_commerce/routes.py
@@ -1,7 +1,9 @@
+from typing import Annotated
+
 import httpx
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Path, Form
 from starlette.requests import Request
-from starlette.responses import HTMLResponse
+from starlette.responses import HTMLResponse, RedirectResponse
 from starlette.templating import Jinja2Templates
 
 templates = Jinja2Templates(directory="online_commerce/templates")
@@ -32,7 +34,7 @@ async def catalogue(request: Request):
 
     cats = response.json()
 
-    return templates.TemplateResponse("catalogue.html", {"request": request, "cats" : cats})
+    return templates.TemplateResponse("catalogue.html", {"request": request, "cats": cats})
 
 
 @html_router.get("/masters", response_class=HTMLResponse)
@@ -41,6 +43,38 @@ async def masters(request: Request):
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
 
-    masters_data = response.json()  # Предполагается, что данные в формате JSON
+    masters_data = response.json()
 
     return templates.TemplateResponse("masters.html", {"request": request, "masters": masters_data})
+
+
+@html_router.get("/areas", response_class=HTMLResponse)
+async def areas(request: Request):
+    url = "http://127.0.0.1:8000/areas"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+
+    areas_data = response.json()
+
+    return templates.TemplateResponse("areas.html", {"request": request, "areas": areas_data})
+
+
+@html_router.get("/services/{id}", response_class=HTMLResponse)
+async def services(request: Request, id: Annotated[int, Path()]):
+    url = f"http://127.0.0.1:8000/services/area/{id}"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+
+    services_data = response.json()
+
+    return templates.TemplateResponse("services.html", {"request": request, "services": services_data})
+
+
+@html_router.post("/register")
+async def register(request: Request, email: str = Form(...), password: str = Form(...)):
+    print(email, password)
+    return RedirectResponse(url="/login", status_code=303)
+
+@html_router.post("/login")
+async def login(request: Request, email: str = Form(...), password: str = Form(...)):
+    return RedirectResponse(url="/", status_code=303)
